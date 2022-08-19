1f1e99049aa698fa3f1438779c0ff64726d53f82
leekibaek <109719834+leekibaek@users.noreply.github.com>
2022-08-19 10:08:19 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+
+
+v1_df = pd.read_csv("V1.CSV")
+
+st.title("Suzuki Violin Vol.1")
+
+file_paths = v1_df["파일명"].to_list()
+titles = v1_df["제목"].to_list()
+tracks = v1_df["트랙번호"].to_list()
+composers = v1_df["작곡자"].to_list()
+tracks_nums = len(tracks)
+
+tabs = st.tabs(tracks)
+
+for i in range(0, tracks_nums):
+    with tabs[i]:
+        col1, col2 = st.columns(2)
+        with col2:
+            st.header("곡듣기")
+            st.write(f"{tracks[i]}: {titles[i]}")
+            st.audio("./audio_files/V1/" + file_paths[i])
+        with col1:
+            st.header("작곡자 정보")
+            st.write(f"작곡자 : {composers[i]}")
+            st.write("(상세 내용은 업데이트 예정)")
+        
+
+# with tabs[0]:
+#     "sdfsdf"
+
+# with tabs[1]:
+#     "tsdd"
+# for i, title, file_path in enumerate(titles, file_paths):
+#     with tabs[i]:
+#         st.write(f"곡제목 : {title}")
+#         st.audio(file_path)
+
+# st.tabs(tracks)
+# st.tabs(tracks_nums)
+# for i, track in enumerate(tracks):
+    
